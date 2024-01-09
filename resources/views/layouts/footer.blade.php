828dcbcb5baa8d2f12f7033380d878dcc2eb4f33
brijpatel1611 <bhadjabrij@gmail.com>
2024-01-09 14:46:59 +0530

    done
    done

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -20,10 +20,13 @@
 <script src="//cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 
 
-<script>
+
+
+
+{{-- <script>
     $(document).ready(function() {
         $('#myTable').DataTable();
     });
-</script>
+</script> --}}
 </body>
 </html>
